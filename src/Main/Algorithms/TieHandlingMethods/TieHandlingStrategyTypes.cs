e56fbf16e8658c54387917dd53f30d24975abe5d
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 15:57:04 -0500

    Initial Commit

---
+++ src/Main/Algorithms/TieHandlingMethods/TieHandlingStrategyTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace USC.GISResearchLab.Geocoding.Core.Algorithms.TieHandlingMethods
+{
+    [Serializable]
+    public class TieHandlingStrategyTypes
+    {
+
+        public static DataTable GetAllTieHandlingStrategyTypes()
+        {
+            DataTable ret = new DataTable();
+            ret.Columns.Add(new DataColumn("value", typeof(string)));
+
+            foreach (TieHandlingStrategyType item in Enum.GetValues(typeof(TieHandlingStrategyType)))
+            {
+                DataRow row = ret.NewRow();
+                row["value"] = item.ToString();
+                ret.Rows.Add(row);
+            }
+
+            return ret;
+        }
+
+    }
+}
